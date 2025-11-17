868fbeae358cbafa7f436f37b1cf7a5ad077f9a4
joonsol <71648638+joonsol@users.noreply.github.com>
2025-11-17 21:50:57 +0900

    feat: homepage setting

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,12 +3,14 @@ import HeroSection from "../../components/home/HeroSection";
 import PopularDestinations from "../../components/home/PopularDestinations";
 import TravelMore from "../../components/home/TravelMore";
 import Newsletter from "../../components/home/Newsletter";
+import HeroSearchWrap from "../../components/home/HeroSearchWrap";
 import "../../styles/pages/home/HomePage.scss";
 
 const HomePage = () => {
  return (
-  <div className="home-page">
+  <div className="home-page top-container">
    <HeroSection />
+   <HeroSearchWrap />
    <PopularDestinations />
    <TravelMore />
    <Newsletter />
